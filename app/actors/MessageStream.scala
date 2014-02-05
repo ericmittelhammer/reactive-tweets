7fd0a2a91ce0972ef5e026cc61551f2bfee5b1c3
eric <code@ericmittelhammer.com>
2014-02-04 20:39:16 -0500

    making delay parameterized.  changing message name

--- app/actors/MessageStream.scala
+++ app/actors/MessageStream.scala
@@ -9,11 +9,13 @@ import akka.event.{ EventStream, Logging, LoggingReceive }
 
 import scala.concurrent.duration._
 
+trait MessageStream
+
 object MessageStream {
 
   type Message = String
 
-  case class Broadcast(message: Message)
+  case class NewMessage(message: Message)
 
   case class StopStream()
 
@@ -23,9 +25,14 @@ object MessageStream {
 
 }
 
-trait MessageStream
+object OfflineMessageStream {
 
-class OfflineMessageStream(supervisor: ActorRef, messageList: List[MessageStream.Message]) extends Actor with MessageStream {
+  def props(supervisor: ActorRef, messages: List[MessageStream.Message], minMilliseconds: Int, maxMilliseconds: Int) =
+    Props(classOf[OfflineMessageStream], supervisor, messages, minMilliseconds, maxMilliseconds)
+
+}
+
+class OfflineMessageStream(supervisor: ActorRef, messageList: List[MessageStream.Message], minMilliseconds: Int, maxMilliseconds: Int) extends Actor with MessageStream {
 
   import MessageStream._
 
@@ -39,21 +46,21 @@ class OfflineMessageStream(supervisor: ActorRef, messageList: List[MessageStream
     case StartStream() => {
       log.info("Stream Started")
       context.become(started)
-      self ! Broadcast(i.next) //send the first message to myself
+      self ! NewMessage(i.next) //send the first message to myself
     }
     case StopStream() => log.warning("Stream already stopped")
-    case Broadcast(message: Message) => log.warning("Trying to brodcast to a stopped stream")
+    case NewMessage(message: Message) => log.warning("Trying to brodcast to a stopped stream")
   }
 
   def started: Receive = LoggingReceive {
     case StartStream() => log.warning("Stream already started")
     case StopStream => context.become(stopped)
-    case Broadcast(message: Message) => {
+    case NewMessage(message: Message) => {
       if (!i.hasNext) i = messageList.iterator // reset the iterator if we've reached the end of the list
-      supervisor ! SocketEndpoint.NewMessage(message) //send the message to the supervisor
-      val nextMessageAt = scala.util.Random.nextInt(2500) + 50
+      supervisor ! NewMessage(message) //send the message to the supervisor
+      val nextMessageAt = scala.util.Random.nextInt((maxMilliseconds - minMilliseconds) + 1) + minMilliseconds
       context.system.scheduler.scheduleOnce(nextMessageAt milliseconds) { //schedule the next message to be sent
-        self ! Broadcast(i.next)
+        self ! NewMessage(i.next)
       }
     }
   }
--- test/MessageStreamSpec.scala
+++ test/MessageStreamSpec.scala
@@ -41,7 +41,7 @@ class OfflineMessageStreamSpec extends TestKit(ActorSystem("OMSSystem",
     shutdown(system)
   }
 
-  val oms = system.actorOf(Props(classOf[OfflineMessageStream], self, List("one", "two", "three")), "oms")
+  val oms = system.actorOf(OfflineMessageStream.props(self, List("one", "two", "three"), 50, 3000), "oms")
 
   "an unstarted OfflineMessageStream Actor" should {
     "not publish any messages" in {
@@ -52,15 +52,14 @@ class OfflineMessageStreamSpec extends TestKit(ActorSystem("OMSSystem",
 
   "an started OfflineMessageStream Actor" should {
     "publish a message within 3 seconds" in {
-      //oms ! MessageStream.Subscribe(testActor)
       oms ! MessageStream.StartStream()
-      expectMsgAllClassOf(3000 milliseconds, classOf[SocketEndpoint.NewMessage])
+      expectMsgAllClassOf(3000 milliseconds, classOf[MessageStream.NewMessage])
     }
   }
 
   "an running OfflineMessageStream Actor" should {
-    "keep publishing" in {
-      receiveN(4, 9000 milliseconds)
+    "keep publishing after it has exhausted list of messages" in {
+      receiveN(4, 12000 milliseconds)
     }
   }
 
